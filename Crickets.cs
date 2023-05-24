0a51b0f538616eed211f9fc3a7af336622504590
smachni <134525104+smachni@users.noreply.github.com>
2023-05-24 21:12:52 +0600

    Add files via upload
    крикетыыыыыыыыы

---
+++ Crickets.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    void Start()
+    {
+        print("Crickets");
+        //пишем тект который будет вводиться во время запуска
+    }
+    void Update()
+    {
+        Debug.Log("Crickets");
+        //повторяем тоже самое дело но с Debug.Log
+    }
+}
